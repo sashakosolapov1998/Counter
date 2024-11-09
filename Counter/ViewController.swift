0ef529f79f70dbf6956eef6c4ce199890d9a2b81
AleksKosolapov <sashakosolapov1998@yandex.ru>
2024-11-09 16:36:53 +0300

    Проект доделан

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+// MARK: Action and Outlet
+    @IBOutlet weak var Label: UILabel!
+    // создаем переменную в которой будем хранить количество нажатий
+    var count : Int = 0
+    
+    @IBAction func Button(_ sender: UIButton) {
+        count += 1
+        // выводим текст и значение в лейбл
+        Label.text = "Значение счетчика:\n \(count)"
+    }
+    
+    
+    
 }
 
